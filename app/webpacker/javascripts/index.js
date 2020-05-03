71472621e29b403200b314c666d975ccf6b5b413
Rei <chloerei@gmail.com>
2020-05-03 21:09:24 +0800

    Add campo-ui

--- app/webpacker/javascripts/index.js
+++ app/webpacker/javascripts/index.js
@@ -8,3 +8,9 @@ import Turbolinks from "turbolinks"
 Turbolinks.start()
 
 import './channels'
+
+import { application } from 'campo-ui/dist/js/campo-ui'
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const context = require.context("./controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))

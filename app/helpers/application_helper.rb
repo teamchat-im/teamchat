9d9e8c25351060027399e0499012f3937beb2636
Rei <chloerei@gmail.com>
2020-05-05 17:26:01 +0800

    Add custom form builder

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def custom_form_with(model: nil, scope: nil, url: nil, format: nil, **options, &block)
+    options.merge!(builder: CustomFormBuilder)
+    options[:html] ||= {}
+    options[:html].merge!(class: 'form')
+    form_with(model: model, scope: scope, url: url, format: format, **options, &block)
+  end
 end
---
+++ lib/custom_form_builder.rb
@@ -0,0 +1,23 @@
+class CustomFormBuilder < ActionView::Helpers::FormBuilder
+  def wrap_field(attribute, options = {})
+    klass = ['form__field']
+    klass << 'form__field--invalid' if object.errors[attribute].any?
+    @template.content_tag(:div, class: klass) do
+      @template.label(@object_name, attribute) +
+      yield +
+      if object.errors[attribute].any?
+        @template.content_tag(:div, object.errors[attribute].first, class: 'form__message')
+      elsif options[:helper_text]
+        @template.content_tag(:div, options[:helper_text], class: 'form__message')
+      end
+    end
+  end
+
+  [:text_field, :password_field].each do |method|
+    define_method(method) do |attribute, options = {}|
+      wrap_field(attribute, options) do
+        super(attribute, options)
+      end
+    end
+  end
+end

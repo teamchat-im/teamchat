f072c73bec60b03a62506e823ddc098473f9d445
Rei <chloerei@gmail.com>
2020-05-06 20:16:37 +0800

    Fix home sign in require

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  before_action :require_signed_in
+
   def index
   end
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,8 +2,13 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
+    sign_in create(:user)
     get root_url
     assert_response :success
   end
 
+  test "should redirect sign in" do
+    get root_url
+    assert_redirected_to sign_in_url(return_to: '/')
+  end
 end
